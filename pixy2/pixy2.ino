86fd0faa1b73dd3fcdfa083a143fe53e328f2fc0
Lim Hyeonjong <78941234+lim-mortan@users.noreply.github.com>
2022-01-30 17:55:28 +0900

    start

---
+++ pixy2/pixy2.ino
@@ -0,0 +1,32 @@
+#include <Pixy2I2C.h>
+Pixy2I2C pixy;
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  Serial.print("Starting...\n");
+  
+  pixy.init();
+}
+ 
+void loop() {
+ 
+  int i; 
+  // grab blocks!
+  pixy.ccc.getBlocks();
+  
+  // If there are detect blocks, print them!
+  if (pixy.ccc.numBlocks)
+  {
+    Serial.print("Detected ");
+    Serial.println(pixy.ccc.numBlocks);
+    for (i=0; i<pixy.ccc.numBlocks; i++)
+    {
+      Serial.print("  block ");
+      Serial.print(i);
+      Serial.print(": ");
+      pixy.ccc.blocks[i].print();
+    }
+  }  
+ 
+}
